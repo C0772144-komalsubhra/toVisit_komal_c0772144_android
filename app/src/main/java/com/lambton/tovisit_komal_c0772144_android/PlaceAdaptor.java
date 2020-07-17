f61f4f784a87789a49e4a4d9d20c0bbefe09cb51
komalcuty40@gmail.com <komalsubhra6768@gmail.com>
2020-07-17 19:05:32 -0400

    created place adaptor

--- app/src/main/java/com/lambton/tovisit_komal_c0772144_android/PlaceAdaptor.java
+++ app/src/main/java/com/lambton/tovisit_komal_c0772144_android/PlaceAdaptor.java
@@ -1,8 +1,57 @@
 package com.lambton.tovisit_komal_c0772144_android;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 import java.util.List;
 
-class PlaceAdaptor {
-    public PlaceAdaptor(MainActivity mainActivity, Object p1, List<Place> placeList, DatabaseHelper mDatabase) {
+public class PlaceAdaptor extends ArrayAdapter {
+    Context context;
+    List<Place> places;
+    int layoutRes;
+    //SQLiteDatabase mDatabase;
+    DatabaseHelper mDatabase;
+
+    public PlaceAdaptor(@NonNull Context context, int resource, List<Place> places, DatabaseHelper mDatabase) {
+        super(context, resource, places);
+        this.context = context;
+        this.places = places;
+        this.layoutRes = resource;
+        this.mDatabase = mDatabase;
     }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View v = inflater.inflate(layoutRes, null);
+
+        TextView tvName = v.findViewById(R.id.name_ID);
+
+
+
+
+        final Place p = places.get(position);
+        tvName.setText(p.getName());
+
+
+        if (p.getVisited()){
+            ImageView i = v.findViewById(R.id.placeImage);
+            i.setImageResource(R.drawable.beenhere);}
+
+        return v;
+    }
+
+
 }
