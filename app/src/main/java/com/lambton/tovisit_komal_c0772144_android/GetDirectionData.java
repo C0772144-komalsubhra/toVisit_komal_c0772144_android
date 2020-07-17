688e92b2f8563534fbe5614b0c1675955f56e3c2
komalcuty40@gmail.com <komalsubhra6768@gmail.com>
2020-07-17 19:16:53 -0400

    working on ui

---
+++ app/src/main/java/com/lambton/tovisit_komal_c0772144_android/GetDirectionData.java
@@ -0,0 +1,34 @@
+package com.lambton.tovisit_komal_c0772144_android;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.IOException;
+
+import static android.content.ContentValues.TAG;
+
+public class GetDirectionData extends AsyncTask<Object,String,String> {
+    String directionData, url;
+    GoogleMap mMap;
+
+    String distance, duration;
+
+    LatLng latLng;
+
+    @Override
+    protected String doInBackground(Object... objects) {
+        Log.i(TAG, "doInBackground: GET DIRECTION DATA ");
+        mMap = (GoogleMap) objects[0];
+        url = (String) objects[1];
+        latLng = (LatLng) objects[2];
+
+        FetchURL fetchURL= new FetchURL();
+        try{
+            directionData = fetchURL.readURL(url);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        return directionData;
+    }
+}
