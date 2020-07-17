7c2936bce09a63ed62be8398fa62ce03307f085d
komalcuty40@gmail.com <komalsubhra6768@gmail.com>
2020-07-17 19:04:01 -0400

    added place model class

--- app/src/main/java/com/lambton/tovisit_komal_c0772144_android/Place.java
+++ app/src/main/java/com/lambton/tovisit_komal_c0772144_android/Place.java
@@ -1,4 +1,58 @@
 package com.lambton.tovisit_komal_c0772144_android;
 
-public class Place {
+import java.io.Serializable;
+
+public class Place implements Serializable {
+    private int id;
+    private String name;
+    private Boolean isVisited;
+    private Double lat, lng;
+
+    public Place(int id, String name, Boolean isVisited, Double lat, Double lng) {
+        this.name = name;
+        this.isVisited = isVisited;
+        this.lat = lat;
+        this.lng = lng;
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getVisited() {
+        return isVisited;
+    }
+
+    public void setVisited(Boolean visited) {
+        isVisited = visited;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    public Double getLng() {
+        return lng;
+    }
+
+    public void setLng(Double lng) {
+        this.lng = lng;
+    }
 }
